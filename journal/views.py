d94c62bada100c5484f448e8ccd39be7b7080cc5
whtai <winnietai60326@gmail.com>
2020-08-23 22:44:26 +0800

    Change fonts and minor layout

--- journal/views.py
+++ journal/views.py
@@ -6,7 +6,7 @@ from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'journal/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
