d4ed23bfad8593d6638d4b81258866b928b0e33f
whtai <winnietai60326@gmail.com>
2020-08-30 20:46:26 +0800

    Add user signup page

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('journal.urls')),
